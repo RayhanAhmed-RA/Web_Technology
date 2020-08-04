ab7fc56d4325a0b77e6ed24ef0e2955fc87fe870
RayhanAhmed-RA <68095894+RayhanAhmed-RA@users.noreply.github.com>
2020-08-04 20:42:58 +0600

    Add files via upload

---
+++ Lab Mini Project/home.php
@@ -0,0 +1,26 @@
+<?php
+	session_start();
+	
+	if(isset($_SESSION['id'])){
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+</head>
+<body>
+
+	<h1>Welcome Home <?=$_SESSION['id']?></h1> 
+	<a href="logout.php">logout</a>
+</body>	
+</html>
+
+
+
+<?php		
+	}else{
+		header('location: login.html');
+	}
+
+?>
